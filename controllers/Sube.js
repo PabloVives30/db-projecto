762e683b160bc448f7f066e5ab7a77bc96e0b720
PabloVives30 <48116412@est.ort.edu.ar>
2024-11-12 14:24:37 -0300

    Update ingresarSube

--- controllers/Sube.js
+++ controllers/Sube.js
@@ -9,20 +9,23 @@ const ingresarSube = async (req, res) => {
         console.log("nroSube:", nroSube);
         console.log("tipo:", tipo);
 
-
         if (!userId || !nroSube) {
+            console.error("Error: Faltan datos (userId o nroSube)");
             return res.status(400).json({ error: "Datos faltantes: userId o nroSube no proporcionados" });
         }
 
         const query = 'INSERT INTO sube (id_usuario, "numeroSube", tipo) VALUES ($1, $2, $3) RETURNING *';
         const result = await pool.query(query, [userId, nroSube, tipo]);
 
+        console.log("Resultado de la inserción:", result.rows);
+        
         res.json({ success: true, message: "Número de Sube ingresado correctamente", data: result.rows });
     } catch (error) {
-        console.error("Error al ingresar la sube", error);
-        res.status(500).json({ error: "Error al ingresar el número de Sube" });
+        console.error("Error en el servidor al ingresar la sube:", error);
+        res.status(500).json({ error: "Error en el servidor al ingresar el número de Sube" });
     }
 };
+
 
 const traersube = async (req, res) => {
     try {
